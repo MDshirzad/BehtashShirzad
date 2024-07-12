9a29c11f413d46bef73e063c3ef08de4bf22da69
Mohamad <80650043+MDshirzad@users.noreply.github.com>
2024-07-12 23:16:52 +0400

    Update MelliPayamakCred.cs

--- ApiCommunicator/DTO/Mellipayamak/MelliPayamakCred.cs
+++ ApiCommunicator/DTO/Mellipayamak/MelliPayamakCred.cs
@@ -8,9 +8,9 @@ namespace ApiCommunicator.DTO.Mellipayamak
 {
     internal class MelliPayamakCred : object
     {
-        public static string MeliPayamakUsername { get; set; } = "09376794095";
-        public static string Number { get; set; } = "50002710094095";
-        public static string MeliPayamakPass { get; set; } = "Mohamadsh81@";
+        public static string MeliPayamakUsername { get; set; } = "";
+        public static string Number { get; set; } = "";
+        public static string MeliPayamakPass { get; set; } = "";
 
     }
 }
